327be463bbbfd05fb3d0caee587f37df0fa94b03
monysary <sary.mony@gmail.com>
2022-12-05 15:56:28 -0800

    declared dependencies, added questions array, partially filled out generateMarkdown.js file

--- index.js
+++ index.js
@@ -1,10 +1,63 @@
-// TODO: Include packages needed for this application
+// Declare dependencies
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Terminal prompts for generating README.md
+const questions = [
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Write a description for your project.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is the command for users to install dependencies?",
+        name: "installation",
+        default: "npm i inquirer@8.2.4"
+    },
+    {
+        type: "input",
+        message: "Provide instructions for use.",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "List your contributors and collaborators.",
+        name: "credits"
+    },
+    {
+        type: "list",
+        message: "What is your project license?",
+        name: "license",
+        choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "NONE"]
+    },
+    {
+        type: "input",
+        message: "What is the command for users to test your project?",
+        name: "test",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+const writeToFile = (fileName, data) => {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,12 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const renderLicenseBadge = license =>
+  license === "NONE" ?  "" : `![${license} License Badge](https://img.shields.io/badge/license-${license}-blue.svg)`;
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = license =>
+  license === "NONE" ? "" : `\n* [License](#License)\n`;
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
